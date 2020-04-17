873f0e94bbfede71679e1d99515d691cf5de7969
fechanique <felipe.echanique@gmail.com>
2020-04-17 11:41:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ try {
 function installSFDX(){
   var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz -P /tmp'
   var createDir = 'mkdir /tmp/sfdx'
-  var unzip = 'tar xJf /tmp/sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1'
-  var install = 'tmp/sfdx/install'
+  var unzip = 'tar xJf /tmp/sfdx-linux-amd64.tar.xz -C /tmp/sfdx --strip-components 1'
+  var install = 'sh tmp/sfdx/install'
   exec(download+' && '+createDir+' && '+unzip+' && '+install, function(error, stdout, stderr){
     if(error) throw(stderr)
     core.debug(stdout)
