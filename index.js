7f6f1785e84b9428868db162441ca867f0465fcd
fechanique <felipe.echanique@gmail.com>
2022-03-31 11:12:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ try {
 
 function installSFDX(){
   var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz -q -P /tmp'
-  var createDir = 'mkdir /sfdx'
-  var unzip = 'tar xJf /tmp/sfdx-linux-x64.tar.xz -C /sfdx --strip-components 1'
-  var install = 'echo "`pwd`/sfdx/bin" >> $GITHUB_PATH'
-  var version = '/sfdx/bin/sfdx --version && /sfdx/bin/sfdx plugins --core'
+  var createDir = 'mkdir /tmp/sfdx'
+  var unzip = 'tar xJf /tmp/sfdx-linux-x64.tar.xz -C /tmp/sfdx --strip-components 1'
+  var install = 'echo "`pwd`/tmp/sfdx/bin" >> $GITHUB_PATH'
+  var version = '/tmp/sfdx/bin/sfdx --version && /tmp/sfdx/bin/sfdx plugins --core'
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+version, function(error, stdout, stderr){
     if(error) throw(stderr)
     core.info(stdout)
@@ -28,7 +28,7 @@ function createAuthFile(){
 
 function authSFDX(){
   var params = '--setdefaultdevhubusername --setdefaultusername -a SFDX-ENV'
-  exec('/sfdx/bin/sfdx auth:sfdxurl:store -f /tmp/sfdx_auth.txt '+params, function(error, stdout, stderr){
+  exec('/tmp/sfdx/bin/sfdx auth:sfdxurl:store -f /tmp/sfdx_auth.txt '+params, function(error, stdout, stderr){
     if(error) throw(stderr)
 	core.info(stdout)
   })
