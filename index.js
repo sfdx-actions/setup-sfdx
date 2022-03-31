b64a42ce5bd6c2e2dbb868cc8e8ed450075cea96
fechanique <felipe.echanique@gmail.com>
2022-03-31 11:01:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function installSFDX(){
   var download = 'wget https://developer.salesforce.com/media/salesforce-cli/sfdx/channels/stable/sfdx-linux-x64.tar.xz -q -P /tmp'
   var createDir = 'mkdir ~/sfdx'
   var unzip = 'tar xJf /tmp/sfdx-linux-x64.tar.xz -C ~/sfdx --strip-components 1'
-  var install = 'echo "~/sfdx/bin" >> $GITHUB_PATH'
+  var install = 'echo "`pwd`~/sfdx/bin" >> $GITHUB_PATH'
   var version = '~/sfdx/bin/sfdx --version && ~/sfdx/bin/sfdx plugins --core'
   exec(download+' && '+createDir+' && '+unzip+' && '+install+' && '+version, function(error, stdout, stderr){
     if(error) throw(stderr)
